e4832419b823c441da9e95723377a38366390d01
Documenter.jl <documenter@juliadocs.github.io>
2023-06-07 20:28:46 +0000

    build based on c00fdaa

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PowerSpectrumNoises","category":"page"},{"location":"#PowerSpectrumNoises","page":"Home","title":"PowerSpectrumNoises","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PowerSpectrumNoises.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PowerSpectrumNoises]","category":"page"}]
+}
