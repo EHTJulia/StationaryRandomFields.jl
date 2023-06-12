07ec8149b3e897df8d2d4d2a5738a4f072a887ef
Anna Tartaglia <art5584@psu.edu>
2023-06-12 14:44:56 -0400

    created singlepowerlaw1d data type and defined 4 basic abstractpowerspectrum methods

--- src/powerspectra/abstractpowerspectrum.jl
+++ src/powerspectra/abstractpowerspectrum.jl
@@ -4,7 +4,8 @@
 This is an abstract data type for noise power spectra.
 
 # Mandatory fields
-
+- `amp::Float64`: amplitude of power spectrum
+- `index::Float64`: power law index β 
 # Mandatory methods
 
 """
@@ -16,3 +17,32 @@ Base.ndims(::NoisePowerSpectrum1D) = 1
 
 abstract type NoisePowerSpectrum2D <: AbstractNoisePowerSpectrum end
 Base.ndims(::NoisePowerSpectrum2D) = 2
+
+
+#
+# Method to compute given amplitude power law function of single point signal
+#
+@inline function map_ampspectrum_point(psmodel::AbstractNoisePowerSpectrum, singleν::Number)::Number #is number data type correct here
+    return psmodel.amp * singleν^(psmodel.index) 
+end
+
+#
+# Method to compute given amplitude power law function of signal grid 
+#
+@inline function map_ampspectrum(psmodel::AbstractNoisePowerSpectrum, gridofν::Tuple)::Tuple # should i define this for other data types
+    return (singleν -> psmodel.amp .* singleν .^ psmodel.index).(singleν)
+end
+
+#
+# Method to compute given power law function of signal grid 
+#
+@inline function map_powerspectrum_point(psmodel::AbstractNoisePowerSpectrum, singleν::Number)::Number
+    return map_ampspectrum_point(psmodel, singleν)^2
+
+#
+# Method to compute given amplitude power law function of signal grid 
+#
+@inline function map_powerspectrum(psmodel::AbstractNoisePowerSpectrum, gridofν::Tuple)::Tuple # should i define this for other data types
+    ampspec = map_ampspectrum(psmodel,gridofν)
+    return (ampspec -> ampspec .^ 2).(ampspec)
+end
---
+++ src/powerspectra/singlepowerlaw1d.jl
@@ -0,0 +1,11 @@
+export SinglePowerLaw1D
+"""
+    SinglePowerLaw1D <: NoisePowerSpectrum1D
+
+This is a data type for a 1 dimensional power law function of the form P(ν)=amp*ν^(index)
+"""
+
+struct SinglePowerLaw1D <: NoisePowerSpectrum1D 
+    amp::Float64
+    index::Float64
+end
