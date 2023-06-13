bddd7c98d626b907cb1023455ea1a14a3ab10f8b
Anna Tartaglia <art5584@psu.edu>
2023-06-13 09:12:39 -0400

    changed order of include

--- src/PowerSpectrumNoises.jl
+++ src/PowerSpectrumNoises.jl
@@ -7,9 +7,9 @@ using Random
 using Statistics
 
 # Noise Signal Models
-include("./noisesignals/abstractnoisesignal.jl")
 include("./noisesignals/noisesignal1d.jl")
 include("./noisesignals/noisesignal2d.jl")
+include("./noisesignals/abstractnoisesignal.jl")
 
 # Power Spectrum Models
 include("./powerspectra/abstractpowerspectrum.jl")
